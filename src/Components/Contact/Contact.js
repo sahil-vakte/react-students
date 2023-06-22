32405a269e0efcf7ef9e5f99c7e2ff98c5176a5c
Sahil <sahilvaktepatil123@gmail.com>
2023-06-22 21:02:50 +0530

    Changes in Contact Component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-    <h1 style={{textAlign:"center"}}>This is Our <br/> Contact Page</h1>
+    <h1 style={{textAlign:"center"}}>This is Our <br/> Contact Page of Our Application</h1>
     
     </div>
   )
